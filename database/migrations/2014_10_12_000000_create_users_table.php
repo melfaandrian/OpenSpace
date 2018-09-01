f168ceaeb77793ad1c711021afe64dd0b701c430
melfaandrian <melfaandrian@gmail.com>
2018-09-02 01:26:26 +0700

    remove /vendor in gitignore to prevent redownloading dependency

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->string('id')->unique();
             $table->string('name');
+            $table->string('nickname');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('role');
             $table->rememberToken();
             $table->timestamps();
         });
