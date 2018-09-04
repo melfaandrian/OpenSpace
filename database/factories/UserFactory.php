e7b39aab58729ba3b60029223c13348767883584
melfaandrian <melfaandrian@gmail.com>
2018-09-04 22:41:58 +0700

    add occupant and user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
+        'id' => $faker->unique()->numberBetween($min = 750000, $max = 754000),
         'name' => $faker->name,
+        'nickname' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'role' => $faker->numberBetween($min = 0, $max = 2),
+        'remember_token' => str_random(10)
     ];
 });
