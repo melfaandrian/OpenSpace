3e964f6c91f5865bcf513d70f706af1c3e32cb2d
melfaandrian <melfaandrian@gmail.com>
2018-09-09 19:30:08 +0700

    check table available

--- app/Http/Controllers/OccupantController.php
+++ app/Http/Controllers/OccupantController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\Occupant\OccupantResource;
 use App\Model\Occupant;
 use App\Model\Table;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -20,7 +21,13 @@ class OccupantController extends Controller
         return OccupantResource::collection($table->occupants);
     }
 
-    public function occupyTable(Table $table, Request $request) {
+    public function occupyTable(Request $request, Table $table) {
+        if($this->isOccupied($table)) {
+            return response()->json([
+                'errors' => 'This table has been occupied'
+            ],Response::HTTP_BAD_REQUEST);
+        }
+
         $occupant = new Occupant();
         $occupant->table_id = $table->id;
         $occupant->user_id = Auth::id();
@@ -30,5 +37,12 @@ class OccupantController extends Controller
         return response([
             'data' => new OccupantResource($occupant)
         ], Response::HTTP_CREATED);
+    }
+
+    private function isOccupied(Table $table) {
+        $totalOccupant = Occupant::whereDate('created_at', Carbon::today())
+            ->where(['table_id' => $table->id, 'status' => 1])->count();
+
+        return $totalOccupant > 0 ? true : false;
     }
 }
